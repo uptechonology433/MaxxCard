b0ea7dead18760307d20cff344a2842830baf880
Uptech <uptech@uptech.local>
2024-07-17 14:54:44 -0300

    subindo

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,11 @@
 <?php
-
-    require_once './vendor/autoload.php';
+	// header("Access-Control-Allow-Origin: *");
+	// header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+	// header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization");
+	// header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+	// header("Access-Control-Max-Age: 10000");
+    
+	require_once './vendor/autoload.php';
     require_once './env.php';
     require_once './src/slimConfiguration.php';
     require_once './src/jwtAuth.php';
